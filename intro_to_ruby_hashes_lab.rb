bfbfacee834f15054788fbe735f95aa18dec8298
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-09-28 16:07:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -68,7 +68,7 @@ def monopoly_with_fourth_tier
           :shortline_railroad => {
             'mortgage_value' => '$800'
             }
-        },
-        },
+        }end
+        }
         }
 end
