f05aba6d9a86de25d02388f7461b3a30ce54a80a
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-09-28 15:57:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,6 +27,7 @@ def monopoly_with_third_tier
       return { :railroads => {
         :pieces => 4,
         :rent_in_dollars => {
+          :one_piece_owned => 25,
           
         },
         :names => {}
